7c15bbabfecec069fa11c9beea1400cb8a5634f2
m3llad0 <dmelladooliveros@gmail.com>
2022-06-09 20:59:45 -0500

    Vending machine

---
+++ test/vending_machine/core_test.clj
@@ -0,0 +1,7 @@
+(ns vending-machine.core-test
+  (:require [clojure.test :refer :all]
+            [vending-machine.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
